5912abca1dec23283ff4e73f6a238cdfb5b4119a
Jianwei Han <hanjianwei@gmail.com>
2025-04-03 11:03:07 +0800

    feat(data): Fix validation on 512 resolution

--- test.sh
+++ test.sh
@@ -14,14 +14,12 @@ if (($1 == 224)); then
 elif (($1 == 512)); then
   CUDA_VISIBLE_DEVICES=0 python -W ignore test.py --is_savenii \
     --num_classes=6 \
-    --root_path=$gctx_data \
     --volume_path=$gctx_data \
     --list_dir=$data_dir/lists \
     --output_dir=$data_dir/model_out_512 \
+    --max_epochs=150 \
     --img_size=512 \
-    --batch_size=8 \
-    --opts MODEL.PRETRAIN_CKPT './pretrained_ckpt/gcvit_21k_large_512.pth.tar' \
-    MODEL.DROP_PATH_RATE 0.1 \
+    --opts MODEL.DROP_PATH_RATE 0.1 \
     MODEL.GCVIT.EMBED_DIM 192 \
     MODEL.GCVIT.LAYER_SCALE 1e-5 \
     MODEL.GCVIT.NUM_HEADS "[6, 12, 24, 48]" \
